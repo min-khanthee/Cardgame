6b77e22a6e740734c8d3e9f81b6a863a92b7c5bb
Min Khanthee <pkhanthee@gmail.com>
2022-08-02 09:25:29 +1200

    seperate concerns

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Button that when clicked, gets a new deck of cards from the deck of cards API
 
-document.getElementById('deck-btn').addEventListener('click', function () {
+function handleClick() {
   fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
     .then((res) => res.json())
     .then((data) => console.log(data))
-})
+}
+
+document.getElementById('deck-btn').addEventListener('click', handleClick)
