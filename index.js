e256b63e247b27a8cf78127590e45292f5a7b793
Min Khanthee <pkhanthee@gmail.com>
2022-08-07 10:34:23 +1200

    store deck id

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // Button that when clicked, gets a new deck of cards from the deck of cards API
 
+let deckId
+
 function handleClick() {
   fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      console.log(data)
+      deckId = data.deck_id
+      console.log(deckId)
+    })
 }
 
 document.getElementById('deck-btn').addEventListener('click', handleClick)
