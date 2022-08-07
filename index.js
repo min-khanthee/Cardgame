0885f710790f18ee9578b5d41b5acadf53e4a729
Min Khanthee <pkhanthee@gmail.com>
2022-08-08 10:56:35 +1200

    add ability to draw new cards 🃏

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function handleClick() {
 }
 
 document.getElementById('deck-btn').addEventListener('click', handleClick)
+
+document.getElementById('draw-cards').addEventListener('click', () => {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+})
