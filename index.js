ecbe8d94d0321260cfd9267139cd178d88558ffa
Min Khanthee <pkhanthee@gmail.com>
2022-08-08 12:06:40 +1200

    display cards

--- index.js
+++ index.js
@@ -14,8 +14,14 @@ function handleClick() {
 
 document.getElementById('deck-btn').addEventListener('click', handleClick)
 
-document.getElementById('draw-cards').addEventListener('click', () => {
+document.getElementById('new-cards').addEventListener('click', () => {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      console.log(data.cards[0].image)
+      document.getElementById('cards').innerHTML = `
+      <img src=${data.cards[0].image} />
+      <img src=${data.cards[1].image} />
+  `
+    })
 })
