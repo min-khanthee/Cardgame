a653024b5a6b1891bfeb86e3efd6da7112672900
Min Khanthee <pkhanthee@gmail.com>
2022-08-08 13:22:58 +1200

    refactor and display cards centrally

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Button that when clicked, gets a new deck of cards from the deck of cards API
 
 let deckId
+const deckBtn = document.getElementById('deck-btn')
+const newCardBtn = document.getElementById('new-cards')
+const cardsContainer = document.getElementById('cards')
 
 function handleClick() {
   fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
@@ -12,15 +15,17 @@ function handleClick() {
     })
 }
 
-document.getElementById('deck-btn').addEventListener('click', handleClick)
+deckBtn.addEventListener('click', handleClick)
 
-document.getElementById('new-cards').addEventListener('click', () => {
+newCardBtn.addEventListener('click', () => {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then((res) => res.json())
     .then((data) => {
       console.log(data.cards[0].image)
-      document.getElementById('cards').innerHTML = `
+      cardsContainer.children[0].innerHTML = `
       <img src=${data.cards[0].image} />
+  `
+      cardsContainer.children[1].innerHTML = `
       <img src=${data.cards[1].image} />
   `
     })
